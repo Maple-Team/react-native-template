95a189c276308864256e8eb3098a14225aca4d96
liutsing <liutsingluo@gmail.com>
2022-02-14 18:47:55 +0800

    feat: 设备信息

--- src/utils/device.ts
+++ src/utils/device.ts
@@ -1,16 +1,13 @@
 import DeviceInfo from 'react-native-device-info'
-// import AsyncStorage from '@react-native-async-storage/async-storage'
-// import RNAdvertisingId from 'react-native-advertising-id'
-// import NetInfo from '@react-native-community/netinfo'
-// import { getTimeSinceStartup } from 'react-native-startup-time'
+import RNAdvertisingId from 'react-native-advertising-id'
+import NetInfo from '@react-native-community/netinfo'
+import { getTimeSinceStartup } from 'react-native-startup-time'
 import * as RNLocalize from 'react-native-localize'
-// import ScreenBrightness from '@adrianso/react-native-device-brightness'
-// import dayjs from 'dayjs'
-// import NfcManager from 'react-native-nfc-manager'
-// import { NativeModules } from 'react-native'
-// import { errorCaptured } from './util'
+import ScreenBrightness from '@adrianso/react-native-device-brightness'
+import dayjs from 'dayjs'
+import NfcManager from 'react-native-nfc-manager'
 import type { Device } from '@/typings/device'
-
+import { AppModule } from '@/modules'
 const locales = RNLocalize.getLocales()
 const timezone = RNLocalize.getTimeZone()
 
@@ -19,19 +16,17 @@ const timezone = RNLocalize.getTimeZone()
  * 一部分从第三方模块中获取
  * 一部分从全局状态中获取(已经获取的，不需要重复获取)
  */
-const {} = DeviceInfo
-
-export default async function getDeviceInfo(): Promise<Device> {
+const { } = DeviceInfo
+export type FromView = 'anglex' | 'angley' | 'anglez' | 'applyId' | 'googleAdvertisingId'
+  | 'phone' | 'screenSize' | 'deviceId' | 'gpsInfo' | 'idcard' | 'intranetIP' | 'isSimulator' | 'requestIp'
+type PrtialDevice = Omit<Device, FromView>
+export default async function getDeviceInfo(): Promise<PrtialDevice> {
   // 使用Promise.all并行获取
-  const device: Device = {
-    anglex: '',
-    angley: '',
-    anglez: '',
-    applyId: '',
+
+  const device: PrtialDevice = {
     bootTime: '',
     availableMemory: '',
     isAgent: '',
-    googleAdvertisingId: '',
     basebandVersion: '',
     howLongBootTime: '',
     batteryPower: '',
@@ -41,16 +36,10 @@ export default async function getDeviceInfo(): Promise<Device> {
     screenBrightness: '',
     isCommunication: 'N',
     device: '',
-    deviceId: '',
     deviceName: '',
     deviceVersion: '',
     persistentDeviceId: '',
     nfcFunction: 'N',
-    gpsInfo: '',
-    idcard: '',
-    intranetIP: '',
-    isSimulator: 'N',
-    requestIp: '',
     language: locales[0].languageTag,
     mcc: '',
     meid: '',
@@ -65,8 +54,6 @@ export default async function getDeviceInfo(): Promise<Device> {
     numberOfSongs: 0,
     numberOfVideos: 0,
     wifiName: '',
-    phone: '',
-    screenSize: '',
     signalStrength: '',
     startTime: 0,
     systemTime: '',
